44eb2a622f7cb53c77f5ece765c8b405f4841f4e
jgqy93 <jgqy93@gmail.com>
2023-01-11 15:31:11 +0800

    search page can return null results now

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,19 +1,40 @@
 import { useSelector } from "react-redux";
 import searchData from "../Data/SearchResultData";
 import ShowCard from "./ShowCard";
+import { FaRegSadTear } from "react-icons/fa";
+
 function SearchResults() {
   //* for api search -> currently use imported data
   const searchResultsData = useSelector((state) => {
     return state.shows.searchData;
   });
 
+  const searchTerm = useSelector((state) => {
+    return state.shows.searchTerm;
+  });
+
+  let content;
   const renderedSearchResults = searchResultsData.map((result) => {
     return <ShowCard key={result.id} show={result} />;
   });
 
+  if (searchResultsData.length < 1) {
+    content = (
+      <div className="text-white flex flex-col items-center mt-10">
+        {" "}
+        <FaRegSadTear />
+        <h1 className="text-center">
+          Sorry, cannot find shows with title "{searchTerm}"...
+        </h1>
+      </div>
+    );
+  } else {
+    content = renderedSearchResults;
+  }
+
   return (
     <div className="flex flex-row flex-wrap gap-5 m-10 justify-center ">
-      {renderedSearchResults}
+      {content}
     </div>
   );
 }
