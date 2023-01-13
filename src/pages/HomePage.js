253cd69f1928fac0c49d926fd2b6d46263f0591e
jgqy93 <jgqy93@gmail.com>
2023-01-13 12:18:36 +0800

    home page background size updated

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -17,27 +17,27 @@ function HomePage() {
     return state.shows.showArray;
   });
 
-  useEffect(() => {
-    let codeArray = [];
-    const fetchShowsBaseData = async () => {
-      const response = await axios.get(networkUrl);
-      codeArray = response.data.titles.splice(0, 10);
+  // useEffect(() => {
+  //   let codeArray = [];
+  //   const fetchShowsBaseData = async () => {
+  //     const response = await axios.get(networkUrl);
+  //     codeArray = response.data.titles.splice(0, 10);
 
-      const fetchData = await Promise.all(
-        codeArray.map((show) => {
-          const url = `https://api.watchmode.com/v1/title/${show.imdb_id}/details/?apiKey=${key}`;
-          return axios.get(url);
-        })
-      );
-      dispatch(setShows(fetchData));
-    };
-    fetchShowsBaseData();
-  }, []);
+  //     const fetchData = await Promise.all(
+  //       codeArray.map((show) => {
+  //         const url = `https://api.watchmode.com/v1/title/${show.imdb_id}/details/?apiKey=${key}`;
+  //         return axios.get(url);
+  //       })
+  //     );
+  //     dispatch(setShows(fetchData));
+  //   };
+  //   fetchShowsBaseData();
+  // }, []);
 
   console.log(carouselShowArray);
 
   return (
-    <div className="bg-zinc-900 h-screen">
+    <div className="bg-zinc-900 min-h-screen md:min-h-screen lg:min-h-screen">
       <ShowHero />
       <Carousel shows={carouselShowArray} />
       <CarouselImported shows={animeData} />
